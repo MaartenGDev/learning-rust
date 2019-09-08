d7cd62b3eb00bb70cfc6caba0e20339a58d49e0d
MaartenGDev <maarten.grootoonk@gmail.com>
2019-09-08 11:19:49 +0200

    Seperate matches with chunks

--- src/schedule_generator.rs
+++ src/schedule_generator.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::slice::Chunks;
 
 #[derive(Clone, Debug)]
 struct Match {
@@ -16,12 +17,12 @@ impl Match {
 }
 
 pub fn generate() {
-    let teams = ["teamA", "teamB", "TeamC","TeamD", "TeamE", "TeamF", "TeamG", "TeamH"];
+    let teams = ["teamA", "teamB", "TeamC"];
 
 
     let mut teams_played_in_week: Vec<&str> = vec![];
     let mut matches: Vec<Match> = vec![];
-    let mut matches_by_variant: HashMap<u16, Vec<Match>> = HashMap::new();
+    let mut matches_by_variant: HashMap<u32, Vec<Match>> = HashMap::new();
 
     // 0-2
     for (_, team_a) in teams.iter().enumerate() {
@@ -30,11 +31,11 @@ pub fn generate() {
                 continue;
             }
 
-            let variant_collection = matches_by_variant.entry(variant_id as u16).or_insert([].to_vec());
+            let variant_collection = matches_by_variant.entry(variant_id as u32).or_insert([].to_vec());
             variant_collection.push(Match::new(team_a, team_b));
         }
     }
-    let mut match_options: HashMap<&u16, Vec<&Match>> = HashMap::new();
+    let mut match_options: HashMap<&u32, Vec<&Match>> = HashMap::new();
 
     for (team_a_id, match_options_for_team_a) in matches_by_variant.iter() {
         let variant_collection = match_options.entry(team_a_id).or_insert([].to_vec());
@@ -54,6 +55,23 @@ pub fn generate() {
         }
     }
 
-    println!("{:#?}", match_options)
+    let mut match_options_with_unique_start: HashMap<&u32, Chunks<&Match>> = HashMap::new();
+
+    for (team_a_id, match_options_for_team_a) in match_options.iter() {
+        match_options_with_unique_start.insert(team_a_id, match_options_for_team_a.chunks(3));
+    }
+    println!("{:#?}", matches_by_variant);
+
+    for (team_id, matches_chunk) in match_options_with_unique_start {
+        for (chunk_id, matches_in_chunk) in matches_chunk.enumerate() {
+            for match_to_play in matches_in_chunk {
+                println!("round: {}, variant: {} -> {} vs {}", team_id,chunk_id, match_to_play.team_a, match_to_play.team_b)
+            }
+            println!();
+        }
+
+        println!();
+        println!();
+    }
 }
 
