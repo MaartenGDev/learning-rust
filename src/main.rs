4aaeadca24d37788ea8031132dc379fa9fd542f9
MaartenGDev <maarten.grootoonk@gmail.com>
2019-09-15 14:20:27 +0200

    Move scheduler to seperate thread

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ extern crate mime;
 extern crate redis;
 extern crate r2d2_redis;
 
-use crate::structs::{State};
-use redis::{Commands};
+use std::thread;
 
 mod structs;
 mod docker_client;
@@ -19,24 +18,9 @@ mod errors;
 mod scheduling_service;
 
 fn main() -> redis::RedisResult<()> {
-    let client = redis::Client::open("redis://0.0.0.0/")?;
-    let mut con = client.get_connection()?;
-
-    let desired_state_key = "desired_state";
-
-    if !con.exists(desired_state_key)? {
-        con.set(desired_state_key, r#"{"containers": []}"#)?;
-    }
-
-    let fetcher = || -> redis::RedisResult<State>{
-        let raw_json: String = con.get(desired_state_key)?;
-        println!("{}", &raw_json);
-        let desired_state: State = serde_json::from_str(&raw_json).unwrap();
-
-        Ok(desired_state)
-    };
-
-    scheduling_service::run(fetcher);
+    thread::spawn(move || {
+        scheduling_service::run().unwrap();
+    });
     api_service::run();
 
     Ok(())
--- src/scheduling_service.rs
+++ src/scheduling_service.rs
@@ -1,37 +1,57 @@
 use futures::future::lazy;
 use tokio::prelude::Future;
-use crate::structs::{State};
+use crate::structs::State;
 use crate::docker_service;
+use redis::Commands;
+use std::{thread, time};
 
-pub fn run<F>(mut fetcher: F) where F: FnMut() -> redis::RedisResult<State> {
-    let desired_state = fetcher();
+pub fn run() -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://0.0.0.0/").unwrap();
+    let mut con = client.get_connection().unwrap();
 
-    match desired_state {
-        Err(e) => panic!("No state found!, {}", e),
-        Ok(state) => {
-            tokio::run(lazy(|| {
-                let future = docker_service::get_missing_containers(state)
-                    .map(|state| {
-                        println!("missing: {:#?}", state);
+    let desired_state_key = "desired_state";
+    let has_previous_value: bool = con.exists(desired_state_key).unwrap();
 
-                        for container in state.containers {
-                            let future = docker_service::schedule_container(&container)
-                                .map(|start_container| {
-                                    tokio::spawn(start_container.map(|_| {
-                                        println!("Started!");
-                                    }).map_err(|e| eprintln!("Error: {:#?}", e)));
-                                })
-                                .map_err(|e| eprintln!("Error: {:#?}", e));
+    if !has_previous_value {
+        con.set(desired_state_key, r#"{"containers": []}"#)?
+    }
 
-                            tokio::spawn(future);
-                        }
-                    }).map_err(|e| eprintln!("Error: {:#?}", e));
+    loop {
+        let raw_json: String = con.get(desired_state_key).unwrap();
+        let desired_state: State = serde_json::from_str(&raw_json).unwrap();
 
+        tokio::run(lazy(|| {
+            start_missing_containers(desired_state);
+            stop_zombie_containers();
 
-                tokio::spawn(future);
-                Ok(())
-            }));
-        }
-    };
+            Ok(())
+        }));
+
+        thread::sleep(time::Duration::from_secs(4));
+    }
 }
 
+fn start_missing_containers(state: State) {
+    let future = docker_service::get_missing_containers(state)
+        .map(|state| {
+            println!("missing: {:#?}", state);
+
+            for container in state.containers {
+                let future = docker_service::schedule_container(&container)
+                    .map(|start_container| {
+                        tokio::spawn(start_container.map(|_| {
+                            println!("Started!");
+                        }).map_err(|e| eprintln!("Error: {:#?}", e)));
+                    })
+                    .map_err(|e| eprintln!("Error: {:#?}", e));
+
+                tokio::spawn(future);
+            }
+        }).map_err(|e| eprintln!("Error: {:#?}", e));
+
+
+    tokio::spawn(future);
+}
+
+
+fn stop_zombie_containers() {}
